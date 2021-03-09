12d98535efd4ecf41d3cb6d068e61f3edaf93a7a
Zois Stavrakas <zoiss1999@gmail.com>
2021-03-09 11:55:48 +1100

    Updated Vector2/3/4
    Added basic vector arithmetic functions

--- MathLib/Vector2.cs
+++ MathLib/Vector2.cs
@@ -94,6 +94,69 @@ namespace MathLib
 			this.y = y;
 			this.z = z;
 		}
+
+		// V = V + V --> Vector Addition
+		public static Vector3 operator+ (Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+
+			return result;
+		}
+
+		// V = V - V --> Vector Subtraction
+		public static Vector3 operator- (Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+
+			return result;
+		}
+
+		// V = f * V --> Vector Scaling (float lhs)
+		public static Vector3 operator* (Vector3 lhs, float rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+
+			return result;
+		}
+
+		// V = V * f --> Vector Scaling (float rhs)
+		public static Vector3 operator* (float lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+
+			return result;
+		}
+
+		// f = V.Dot(V) --> Dot Product
+
+
+		// V = V.Cross(V) --> Cross Product
+
+
+		// f = V.Magnitude() --> Magnitude
+
+
+		// Normalise()
+
+
+		// setRotateX(f)
+
+
+		// setRotateY(f)
+
+		// setRotateZ(f)
 	}
 
 
@@ -114,6 +177,75 @@ namespace MathLib
 			this.z = z;
 			this.w = w;
 		}
+
+		// V = V + V --> Vector Addition
+		public static Vector4 operator+ (Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+			result.w = lhs.w + rhs.w;
+
+			return result;
+		}
+
+		// V = V - V --> Vector Subtraction
+		public static Vector4 operator- (Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+			result.w = lhs.w - rhs.w;
+
+			return result;
+		}
+
+		// V = f * V --> Vector Scaling (float lhs)
+		public static Vector4 operator* (Vector4 lhs, float rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+			result.w = lhs.w * rhs;
+
+			return result;
+		}
+
+		// V = V * f --> Vector Scaling (float rhs)
+		public static Vector4 operator* (float lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+			result.w = lhs * rhs.w;
+
+			return result;
+		}
+
+		// f = V.Dot(V) --> Dot Product
+
+
+		// V = V.Cross(V) --> Cross Product
+
+
+		// f = V.Magnitude() --> Magnitude
+
+
+		// Normalise()
+
+
+		// setRotateX(f)
+
+
+		// setRotateY(f)
+
+		// setRotateZ(f)
+
+		// setRotateW(f)
 	}
 
 
